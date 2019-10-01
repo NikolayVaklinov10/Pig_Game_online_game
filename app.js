dd881e0fa62c68418b44235e749e6cea906ee381
Nick <nikolayvaklinov@yahoo.com>
2019-10-01 21:46:59 +0100

    Implementing  'Hold' function and dry principle

--- app.js
+++ app.js
@@ -43,6 +43,39 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
     }else{
         //Next player
+       nextPlayer();
+    }
+
+
+
+});
+
+
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+   // Add current score to global score
+    scores[activePlayer] += roundScore;
+
+   // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+   // Check if player won the game
+    if (scores[activePlayer] >= 20){
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    }else{
+        //Next player
+    nextPlayer();
+
+    }
+
+});
+
+
+function nextPlayer(){
+    //Next player
         activePlayer === 0 ? activePlayer = 1 : activePlayer  = 0;
         roundScore = 0;
 
@@ -56,11 +89,7 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
         document.querySelector('.dice').style.display = 'none';
 
-    }
-
-
-
-});
+}
 
 
 
